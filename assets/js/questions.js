3d7bfa2cad67d02a095b2fbae0b8eeea31eddbe5
Malinchristina <>
2023-12-11 10:02:14 +0000

    Add content to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,138 @@
+// Structure for questions from https://simplestepscode.com/javascript-quiz-tutorial/
+const quizQuestions = [
+    {
+        question: "What is the official language of Sweden?",
+        answers: {
+            a: "Finish",
+            b: "Norwegian",
+            c: "Swedish"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Sweden is known for its extensive archipelago. How many islands does the Stockholm archipelago consist of approximately?",
+        answers: {
+            a: "30 000",
+            b: "40 000",
+            c: "20 000"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "The Nobel Prizes, including the Nobel Peace Prize, are awarded annually in Sweden. In which city are the Nobel Prizes presented?",
+        answers: {
+            a: "Stockholm",
+            b: "Göteborg",
+            c: "Malmö"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What is the currency of Sweden?",
+        answers: {
+            a: "SAR",
+            b: "SEK",
+            c: "KRW"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Who is the Swedish actor known for his roles in films such as ‘Pirates of the Caribbean’ and ‘Mamma Mia!’?",
+        answers: {
+            a: "Stellan Skarsgård",
+            b: "Max von Sydow",
+            c: "Peter Stormare"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "In which year did Sweden officially abolish the driving of cars on the left side of the road to switch to driving on the right?",
+        answers: {
+            a: "1947",
+            b: "1957",
+            c: "1967"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What natural phenomenon is commonly seen in the northern parts of Sweden, where colorful lights dance across the night sky?",
+        answers: {
+            a: "The Swedish lights",
+            b: "The Northern lights",
+            c: "The Dancing lights"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which Swedish singer-songwriter represented Sweden and won the Eurovision Song Contest in 2012 with the song ‘Euphoria’?",
+        answers: {
+            a: "Carola",
+            b: "Loreen",
+            c: "Kicki"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Sweden is known for its high-quality music exports. What popular streaming service was founded in Sweden in 2006?",
+        answers: {
+            a: "Soundcloud",
+            b: "Tidal",
+            c: "Spotify"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "In Sweden, what is the 'Allemansrätten' or 'Everyman's Right' referring to?",
+        answers: {
+            a: "The right of public access to the wilderness, allowing people to move freely in nature.",
+            b: "The right of public access to cut trees, allowing people to cut trees for their own use.",
+            c: "The right of public access to others private lawns, allowing people to use their lawns for recreational use."
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Which Swedish scientist is considered the father of modern taxonomy and developed the system of binomial nomenclature for naming species?",
+        answers: {
+            a: "Alfred Nobel",
+            b: "Carl Von Linné",
+            c: "Carl Von Nobel"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What Swedish city is often referred to as the ‘Venice of the North’ due to its numerous canals and waterways?",
+        answers: {
+            a: "Malmö",
+            b: "Stockholm",
+            c: "Göteborg"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "In the Swedish calendar, what celebration takes place on the 13th of December, featuring a procession of people dressed in white robes and candles?",
+        answers: {
+            a: "St. Lucia's Day",
+            b: "St Patrick's Day",
+            c: "St Mary's Day"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "What day is most common in Sweden to celebrate Christmas on?",
+        answers: {
+            a: "December 23rd",
+            b: "December 24th",
+            c: "December 25th"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Which Swedish author wrote the popular Millennium series, starting with ‘The Girl with the Dragon Tattoo’",
+        answers: {
+            a: "Stieg Larsson",
+            b: "Carl Linné",
+            c: "Alexander Skarsgård"
+        },
+        correctAnswer: "a"
+    },
+];

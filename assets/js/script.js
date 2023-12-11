a9f6694f28a806d31c609bc9d6456dfd44cbed15
Malinchristina <>
2023-12-11 10:12:58 +0000

    Add basic structure to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,44 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+// Check that user name is entered
+function checkUserName()
+
+/**
+ * Modal that shows as popup when user do not enter user name
+ * Modal taken from https://www.w3schools.com/howto/howto_css_modals.asp
+ */
+function userNameModal() {}
+
+// Initiate quiz
+function startQuiz() {}
+
+/**
+ * Function to call questions in random order no matter if there are
+ * new questions added or removed. 
+ */
+function shuffleQuizQuestions() {}
+
+// Show questions
+function showQuestion() {}
+
+// Check selected answer;
+function checkAnswer(event) {}
+
+// Show next question
+function playNextQuestion() {}
+
+// Increment correct score
+function incrementScore() {}
+
+// Increment incorrect score
+function incrementIncorrectScore() {}
+
+// Reset score if player want to play again
+function resetScore() {}
+
+//Restart the game
+function playAgain() {}
+
+//End game if user want to quit beforhand or all questions are played
+function endGame() {}
